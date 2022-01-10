582d36d09dad0f7360797ab7cf95c17ff77c90fa
NelliaS <nelaslezakova@seznam.cz>
2022-01-10 09:48:17 +0100

    change title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ const MyApp = ({ Component, pageProps }: AppProps): any => (
 
 <>
   <Head>
-    <title>Nela Slezáková</title>
+    <title>Nela provází</title>
     <meta name="description" content="Nela Slezáková - personal web" />
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link rel="icon" href="/favicon.ico" />
