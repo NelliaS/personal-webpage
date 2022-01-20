fe4ea17ff99a428c41df4b6bf6cde27ef24cfed5
NelliaS <nelaslezakova@seznam.cz>
2022-01-20 11:16:00 +0100

    modify text about how it works

--- src/pages/jakToFunguje.tsx
+++ src/pages/jakToFunguje.tsx
@@ -22,14 +22,27 @@ const JakToFunguje: NextPage = () => {
         <section className={styles.content__block1} >
           <div className={styles.content__box}>
             <h1>Jak pracuji</h1>            
-             <p>Poskytuju individualizovanou podporu na tvojí cestě za (lepší) prací v IT. Chci ti pomoct se do IT dostat snáze, efektivněji a ve větším klidu. Můžeš si vybrat mezi koučinkem, mentoringem, sebepoznáním a podporou. Během ledna můžeš využít výhody zaváděcích cen. Každé sezení se uskutečňuje pomocí videohovoru (55 min) a nehledě na variantu spolupráce, ti přinese uspořádání myšlenek, nadhled a větší klid. Většině lidí by do úvodu mělo stačit 3-5 sezení. Specifika jednotlivých variant spolupráce najdeš níže, pokud se chceš dozvědět víc, jednoduše využij možnosti nezávazné konzultace (30 min) - pojďme se pobavit o tvém tématu a o tom, co ti můžu nabídnout.</p>
-              </div>       
-        </section >
+             <p>Poskytuju individualizovanou podporu na tvojí cestě za (lepší) prací v IT. Pokud si junior/ka, začni se mnou spolupracovat a dostaň se do IT snáze, efektivněji a ve větším klidu. Pokud v IT pracuješ a nejsi spokojený/á, pojď se mnou zjistit, jak na tom jsi, co potřebuješ a jak to získáš. Ve své praxi stavím jednak na svém psychologickém vzdělání, terapeutických dovednostech (procesově orientovaná práce) a koučinku. Tak na zkušenostech z mojí cesty do IT a zkušenostech z vlastní programátorské praxe. Nabízím Ti 4 varianty spolupráce - můžeš si vybrat mezi koučinkem, mentoringem, sebepoznáním a podporou. Blíže k jednotlivým variantám spolupráce se dozvíš níže.</p>
+              <p>Prakticky to funguje tak, že se objednáš a ve stanovený čas se připojíš na videohovor, o délce 55 minut. Frekvence sezení je zpravidla 1x týdně. Odhadovat potřebný počet sezení je ošemetné, ale zpravidla by lidem mělo do úvodu stačit 3 - 5 sezení. Každé sezení, nehledě na variantu spolupráce, by ti mělo přinášet uspořádání myšlenek, nadhled a větší klid. Nelam si hlavu, pokud nevíš, jaký typ spolupráce je pro tebe ten pravý. Využij možnosti nezávazné konzultace (30 min) - pojďme se pobavit o tvém tématu a tom, co ti můžu nabídnout. </p>
+              <p>Moje služby jsou v lednu dostupné za zaváděcí ceny (výrazně nižší než tržní ceny). Využij toho! </p>
+              
+              </div>     
+
+         </section >
         <section className={styles.content__block2}>
           <h1>Varianty spolupráce</h1>
         </section>
         <section className={styles.content__block3}>
           <div className={styles.card_container}>
+          <div className={styles.card}>
+          <h2>Nezávazná konzultace</h2>
+            <ul>
+              <li>pojď si se mnou popovídat o tvém tématu a o tom, co ti můžu nabídnout</li>
+              <li>získej další informace o jednotlivých variantách spolupráce a o tom, co ti mohou přinést.</li>
+              <li>zeptej se na všechno, co tě zajímá</li>
+              <p>30 minut za 0,- Kč</p>
+            </ul>
+          </div>
           <div className={styles.card}>
             <h2>Koučink</h2>
             <ul>
@@ -66,14 +79,6 @@ const JakToFunguje: NextPage = () => {
               <li>vydáš na cestu, která je pro tebe nejvhodnější</li>
               <li>budeš vědět, na čem stavět úspěch a jak dál</li>
               <p>1 sezení za <del>1500,- Kč</del> 750,-Kč</p>
-            </ul>
-          </div>
-          <div className={styles.card}>
-          <h2>Dlouhodobá</h2>
-            <ul>
-              <li>spolupracuj se mnou dlouhodobě a v každý moment využívej takový typ spolupráce, který ti dá nejvíc</li>
-              <li>získej dlouhodobou podporu na svojí cestě za (lepší) prací v IT</li>
-              <p>Individuální nacenění spolupráce</p>
             </ul>
           </div>
           </div>
