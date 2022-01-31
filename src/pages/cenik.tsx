c5fb3ccf6f605ea5e79685f517cd3a34f24e6c57
radozeman <r.zeman96@gmail.com>
2022-01-31 19:13:23 +0100

    cenik mobile version

--- src/pages/cenik.tsx
+++ src/pages/cenik.tsx
@@ -7,7 +7,7 @@ import HeroImage from "../../public/images/cenik.webp";
 
 const Cenik: NextPage = () => {
     return (
-        <main className={styles.content}>
+        <section className={styles.content}>
             <section className={styles.boxwrapper}>
                 <div className={styles.titlewrapper}>
                     <h1>Ceník</h1>
@@ -33,15 +33,39 @@ const Cenik: NextPage = () => {
                         <p>* k tomu navíc jednorázově 990,- Kč za 2 diagnostické metody a jejich vyhodnocení</p>
                         <p>** zvýhodněná cena pro typ spolupráce, která je zpravidla dlouhodobější</p>
                     </div>
-                    
                         <Link href="/kontaktni-formular">
                             <a><Button value="Chci zjistit víc" type='submit'/></a>
                         </Link>
-                    
                 </section>
             </section>
 
-        </main>
+            {/* MOBILE SECTION */}
+
+            <section className={styles.mobilecontent}>
+                <div className={styles.mobileimgwrapper}></div>
+                <section className={styles.mobilebuttonwrapper}>
+                    <a className={styles.mbitem1}>Koučing</a>
+                    <a className={styles.mbitem2}>Sebepoznání</a>
+                    <a className={styles.mbitem3}>Mentoring</a>
+                    <a className={styles.mbitem4}>Podpora</a>
+                </section>
+                <section className={styles.mobiletablewrapper}>
+                    <h1>Cena za sezení*</h1>
+                    <section className={styles.mobilegridwrapper}>
+                        <p>1. - 3. sezení</p>
+                        <p>4. a další</p>
+                        <p>990,- Kč</p>
+                        <p>890,- Kč</p>
+                    </section>
+                    <p className={styles.mtextalign}>* k tomu navíc jednorázově <span className={styles.mtextinfo}>990,- Kč</span> za dvě diagnostické metody a jejich vyhodnocení</p>
+                </section >
+                <Link href="/kontaktni-formular">
+                    <a className={styles.malign}><Button value="Chci zjistit víc" type='submit'/></a>
+                </Link>
+            </section>
+        </section>
+        
+        
     )
 }
 
