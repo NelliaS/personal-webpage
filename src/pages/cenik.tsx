533f8dac94ebb7c62409bd0f7f3805c7ea4dae85
Nela Slezáková <nela.slezakova@flowerchecker.com>
2022-07-01 15:41:42 +0200

    prices raised due to inflation...

--- src/pages/cenik.tsx
+++ src/pages/cenik.tsx
@@ -9,8 +9,8 @@ import HeroImage from "../../public/images/cenik.webp";
 
 const Cenik: NextPage = () => {
 
-    const [cena1, setCena1] = useState("890,- Kč");
-    const [cena2, setCena2] = useState("790,- Kč");
+    const [cena1, setCena1] = useState("990,- Kč");
+    const [cena2, setCena2] = useState("890,- Kč");
     const [info1, setInfo1] = useState("");
     const [info2, setInfo2] = useState("");
 
@@ -27,15 +27,15 @@ const Cenik: NextPage = () => {
                     <h1 className={styles.griditem3}>Sebepoznání*</h1>
                     <h1 className={styles.griditem4}>Podpora**</h1>
                     <p className={styles.griditem5}>1. - 3. sezení</p>
-                    <p className={styles.griditem6}>890,- Kč</p>
-                    <p className={styles.griditem7}>890,- Kč</p>
-                    <p className={styles.griditem8}>890,- Kč</p>
-                    <p className={styles.griditem9}>790,- Kč</p>
+                    <p className={styles.griditem6}>990,- Kč</p>
+                    <p className={styles.griditem7}>990,- Kč</p>
+                    <p className={styles.griditem8}>990,- Kč</p>
+                    <p className={styles.griditem9}>890,- Kč</p>
                     <p className={styles.griditem10} id=''>4. a další sezení</p>
-                    <p className={styles.griditem11}>790,- Kč</p>
-                    <p className={styles.griditem12}>790,- Kč</p>
-                    <p className={styles.griditem13}>790,- Kč</p>
-                    <p className={styles.griditem14}>690,- Kč</p>
+                    <p className={styles.griditem11}>890,- Kč</p>
+                    <p className={styles.griditem12}>890,- Kč</p>
+                    <p className={styles.griditem13}>890,- Kč</p>
+                    <p className={styles.griditem14}>790,- Kč</p>
                 </section>
                 <section className={styles.flexwrapper}>
                     <div className={styles.textwrapper}>
@@ -56,24 +56,24 @@ const Cenik: NextPage = () => {
                 </div>
                 <section className={styles.mobilebuttonwrapper}>
                     <button onClick={() => {
-                        setCena1("890,- Kč");
-                        setCena2("790,- Kč");
+                        setCena1("990,- Kč");
+                        setCena2("890,- Kč");
                         setInfo1("");
                         setInfo2("");
                         }} className={styles.mbitem1}>Koučink</button>
                     <button onClick={() => {
-                        setCena1("890,- Kč");
-                        setCena2("790,- Kč");
+                        setCena1("990,- Kč");
+                        setCena2("890,- Kč");
                         setInfo1("*");
                         setInfo2("* k tomu navíc jednorázově 990,- Kč za dvě diagnostické metody a jejich vyhodnocení");}} className={styles.mbitem2}>Sebepoznání</button>
                     <button onClick={() => {
-                        setCena1("890,- Kč");
-                        setCena2("790,- Kč");
+                        setCena1("990,- Kč");
+                        setCena2("890,- Kč");
                         setInfo1("");
                         setInfo2("");}} className={styles.mbitem3}>Mentoring</button>
                     <button onClick={() => {
-                        setCena1("790,- Kč");
-                        setCena2("690,- Kč");
+                        setCena1("890,- Kč");
+                        setCena2("790,- Kč");
                         setInfo1("*");
                         setInfo2("* sleva pro dlouhodobější styl spolupráce, v tíživé finanční situaci lze cenu dále uzpůsobit");
                     }} 
